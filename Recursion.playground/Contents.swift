71a9cdc95a00b9f5d5c66d3367c028cc19a931ad
Vladyslav Boiko <bird.identity@gmail.com>
2020-05-25 18:06:53 +0300

    Recursion factorial

--- Recursion.playground/Contents.swift
+++ Recursion.playground/Contents.swift
@@ -9,3 +9,20 @@ func recursionCountdown(n: Int) {
 }
 
 recursionCountdown(n: 6)
+
+func recursionFactorial(n: Int) -> Int {
+    var result = n
+    
+    if n > 2 {
+       result *= recursionFactorial(n: n - 1)
+    }
+    
+    return result
+}
+
+recursionFactorial(n: 1)
+recursionFactorial(n: 2)
+recursionFactorial(n: 3)
+recursionFactorial(n: 4)
+recursionFactorial(n: 5)
+recursionFactorial(n: 6)
